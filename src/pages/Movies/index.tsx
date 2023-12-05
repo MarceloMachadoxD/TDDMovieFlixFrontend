0ba6eef13ff026e8e2cd82e5352fe0ea6a7eb124
Marcelo Machado <marcelo.araujo@evoluservices.com>
2023-12-05 19:04:32 -0300

    Refactor Movies page layout and add movie links

--- src/pages/Movies/index.tsx
+++ src/pages/Movies/index.tsx
@@ -1,11 +1,22 @@
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 const Movies = () => {
   return (
     <>
       <div className="movies-container">
-        <div className="movies-banner">
+        <div className="movies-title">
           <h1>Tela listagem de filmes</h1>
+        </div>
+        <div className="movies-list">
+          <ul>
+            <li>
+              <Link to={'/movies/:movieId'}>Acessar /movies/1</Link>
+            </li>
+            <li>
+              <Link to={'/movies/:movieId'}>Acessar /movies/2</Link>
+            </li>
+          </ul>
         </div>
       </div>
     </>
