309e5e094c7db02f8f5b7c5fed0a1c29af919e94
Marcelo Machado <marcelo.araujo@evoluservices.com>
2023-12-05 18:44:58 -0300

    Add Movies page with styles

---
+++ src/pages/Movies/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css';
+
+const Movies = () => {
+  return (
+    <>
+      <div className="movies-container">
+        <div className="movies-banner">
+          <h1>Tela listagem de filmes</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Movies;
