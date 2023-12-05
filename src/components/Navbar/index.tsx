38a2f88bbca41e80464564aa679cfff6a4082eaa
Marcelo Machado <marcelo.araujo@evoluservices.com>
2023-12-05 18:37:51 -0300

    Add showLogout variable to Navbar component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,11 +3,13 @@ import { NavLink } from 'react-router-dom';
 import './styles.css';
 
 const Navbar = () => {
+  const showLogout = false;
   return (
     <nav className="navbar main-nav">
       <NavLink to="/" className="nav-logo-text">
         <h4 className="text-primary">MovieFlix</h4>
       </NavLink>
+      {showLogout && <button className="logout-button">Sair</button>}
     </nav>
   );
 };
