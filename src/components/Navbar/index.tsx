0f83a0ca35c24b3250160aed0b3af71afe35b45d
Marcelo Machado <marcelo.araujo@evoluservices.com>
2023-12-05 18:18:24 -0300

    Update Navbar logo text

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,14 +1,13 @@
-import { Link } from "react-router-dom";
-import React from "react";
+import { NavLink } from 'react-router-dom';
 
 import './styles.css';
 
 const Navbar = () => {
   return (
     <nav className="navbar main-nav">
-      <Link to="/" className="nav-logo-text">
-        <h4 className="text-primary">NavBar</h4>
-      </Link>
+      <NavLink to="/" className="nav-logo-text">
+        <h4 className="text-primary">MovieFlix</h4>
+      </NavLink>
     </nav>
   );
 };
